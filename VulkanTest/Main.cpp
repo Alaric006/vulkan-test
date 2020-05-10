dd62e2ce8a84908d79e8a2446d2de7807520de5e
Alaric Jenkins <alaricjenkins@outlook.com>
2020-05-10 16:39:15 -0400

    Improved up vector in view matrix

--- VulkanTest/Main.cpp
+++ VulkanTest/Main.cpp
@@ -1527,7 +1527,7 @@ private:
 		}
 		UniformBufferObject ubo = {};
 		ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.view = glm::lookAt(position, position + direction, glm::vec3(0.0f, 0.0f, 1.0f));
+		ubo.view = glm::lookAt(position, position + direction, glm::cross(right, direction));
 		ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1;
 		void* data;
